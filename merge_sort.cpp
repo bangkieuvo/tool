9f75d5701ac113ee284b39e33188265a2dff7b25
ilmvrm <53942882+ilmvrm@users.noreply.github.com>
2020-11-22 02:08:45 +0700

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,24 +1,20 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 void merge(vector<int>&x,int l,int m,int r){
     int i=l;
     int j=m+1;
     vector<int>y;
-    while(i<=m&&j<=r)
-        if(x[i]<x[j]){
+    while(i<=m&&j<=r){
+        if(x[i]<x[j])
             y.push_back(x[i]);
-            i++;
-            }
-        else{
+        else
             y.push_back(x[j]);
-            j++;
-            }
+        j++;
+    }
     while(i<=m){
         y.push_back(x[i]);
         i++;
         }
-    while(j<=r){
+    while(j<=r)
+    {
         y.push_back(x[j]);
         j++;
         }
@@ -31,13 +27,4 @@ void merge_sort(vector<int>&x,int l,int r){
     merge_sort(x,l,m);
     merge_sort(x,m+1,r);
     merge(x,l,m,r);
-}
-
-int main()
-{
-  vector<int>a={8,8,7,4,5,6,1,2,3,1,1,1,100};
-  merge_sort(a,0,a.size()-1);
-  for(auto p=a.begin();p!=a.end();p++)
-    cout<<*p<<" ";
-return 0;
 }
